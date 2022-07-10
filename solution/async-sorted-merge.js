73c8617392fefb965b35602e36be643b4821ed1d
Stephan Elondo <stephanelondo@Stephans-MacBook-Pro.local>
2022-07-10 19:36:12 -0400

    feat: builds out the asyncronous function and extract merge function into a seperate file

--- solution/async-sorted-merge.js
+++ solution/async-sorted-merge.js
@@ -1,9 +1,33 @@
 "use strict";
 
+const merge = require("./merge");
+
+const appendIndex = async (logSource, index) => {
+  let currentLog = await logSource.popAsync();
+  currentLog ? (currentLog.index = index) : (currentLog = false);
+  return currentLog;
+};
+
 // Print all entries, across all of the *async* sources, in chronological order.
 
-module.exports = (logSources, printer) => {
-  return new Promise((resolve, reject) => {
-    resolve(console.log("Async sort complete."));
-  });
+module.exports = async (logSources, printer) => {
+  // append indexes and sort dates in descending order
+  let lastLogEntries = (await Promise.all(logSources.map(appendIndex))).sort(
+    (a, b) => b.date - a.date
+  );
+
+  while (lastLogEntries.length > 0) {
+    let oldestLogEntry = await lastLogEntries.pop();
+    if (oldestLogEntry) {
+      printer.print(oldestLogEntry);
+
+      let logIndex = oldestLogEntry.index;
+      let unsortedLogSource = await appendIndex(logSources[logIndex], logIndex);
+
+      if (unsortedLogSource) {
+        lastLogEntries = merge(unsortedLogSource, lastLogEntries);
+      }
+    }
+  }
+  printer.done();
 };
--- solution/sync-sorted-merge.js
+++ solution/sync-sorted-merge.js
@@ -1,42 +1,6 @@
 "use strict";
-// recursively merge log entries
-const merge = (
-  logEntry,
-  entriesArray,
-  leftIndex = 0,
-  rightIndex = entriesArray.length - 1
-) => {
-  // using unsigned right shift operator for better memory management
-  let midIndex = leftIndex + ((rightIndex - leftIndex) >>> 1);
-  if (entriesArray.length < 1) {
-    return [logEntry];
-  }
-  const entryDate = logEntry.date,
-    leftDate = entriesArray[leftIndex].date,
-    midDate = entriesArray[midIndex].date,
-    rightDate = entriesArray[rightIndex].date;
 
-  if (entryDate >= leftDate) {
-    return [
-      ...entriesArray.slice(0, leftIndex),
-      logEntry,
-      ...entriesArray.slice(leftIndex),
-    ];
-  }
-
-  if (entryDate <= rightDate) {
-    return [
-      ...entriesArray.slice(0, rightIndex + 1),
-      logEntry,
-      ...entriesArray.slice(rightIndex + 1),
-    ];
-  }
-  if (entryDate >= midDate) {
-    return merge(logEntry, entriesArray, leftIndex, midIndex);
-  } else {
-    return merge(logEntry, entriesArray, midIndex + 1, rightIndex);
-  }
-};
+const merge = require("./merge");
 
 const appendIndex = (logSource, index) => {
   let currentLog = logSource.pop();
@@ -64,5 +28,3 @@ module.exports = (logSources, printer) => {
   }
   printer.done();
 };
-
-module.exports.merge = merge;
